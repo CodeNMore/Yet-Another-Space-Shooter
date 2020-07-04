a28f8651fd85bf8eeb9ee7c8566a68e1d693a238
Tony <jackofall55@gmail.com>
2020-07-03 17:15:18 -0700

    Episode 6

---
+++ Player/Player.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+onready var animatedSprite := $AnimatedSprite
+
+export var speed: float = 100
+var vel := Vector2(0, 0)
+
+func _process(delta):
+	# Animate
+	if vel.x < 0:
+		animatedSprite.play("Left")
+	elif vel.x > 0:
+		animatedSprite.play("Right")
+	else:
+		animatedSprite.play("Straight")
+	
+func _physics_process(delta):
+	var dirVec := Vector2(0, 0)
+	
+	if Input.is_action_pressed("move_left"):
+		dirVec.x = -1
+	elif Input.is_action_pressed("move_right"):
+		dirVec.x = 1
+	if Input.is_action_pressed("move_up"):
+		dirVec.y = -1
+	elif Input.is_action_pressed("move_down"):
+		dirVec.y = 1
+
+	vel = dirVec.normalized() * speed
+	position += vel * delta
+	
+	# Make sure that we are within the screen
+	var viewRect := get_viewport_rect()
+	position.x = clamp(position.x, 0, viewRect.size.x)
+	position.y = clamp(position.y, 0, viewRect.size.y)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Yet Another Space Shooter
+A project taught in the "Let's Make a Godot Space Shooter" tutorial series by CodeNMore.
