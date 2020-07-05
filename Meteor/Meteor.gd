bfa9f0e594395067da60c37270a8ed8e18b3c1d1
Tony <jackofall55@gmail.com>
2020-07-05 11:47:34 -0700

    Episode 17

--- Meteor/Meteor.gd
+++ Meteor/Meteor.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var pMeteorEffect := preload("res://Meteor/MeteorEffect.tscn")
+
 export var minSpeed: float = 10
 export var maxSpeed: float = 20
 export var minRotationRate: float = -20
@@ -27,6 +29,10 @@ func _physics_process(delta):
 func damage(amount: int):
 	life -= amount
 	if life <= 0:
+		var effect := pMeteorEffect.instance()
+		effect.position = position
+		get_parent().add_child(effect)
+		
 		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
---
+++ Meteor/MeteorEffect.gd
@@ -0,0 +1,8 @@
+extends CPUParticles2D
+
+func _ready():
+	emitting = true
+
+func _process(delta):
+	if !emitting:
+		queue_free()
