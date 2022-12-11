5396aed1961259db6539e729977d603f38d67243
CodeNMore <jackofall55@gmail.com>
2022-12-11 14:08:40 -0500

    Episode 29 - Powerup Spawning

--- Spawner/Spawner.gd
+++ Spawner/Spawner.gd
@@ -7,29 +7,38 @@ var preloadedEnemies := [
 	preload("res://Enemy/SlowShooter.tscn"),
 	preload("res://Enemy/BouncerEnemy.tscn")
 ]
+var preloadedPowerups := [
+	preload("res://Powerups/ShieldPowerup.tscn"),
+]
 var plMeteor := preload("res://Meteor/Meteor.tscn")
 
 onready var spawnTimer := $SpawnTimer
+onready var powerupSpawnTimer := $PowerupSpawnTimer
 
-var nextSpawnTime := 5.0
+export var nextSpawnTime := 5.0
+export var minPowerupSpawnTime := 3
+export var maxPowerupSpawnTime := 20
 
 func _ready():
 	randomize()
 	spawnTimer.start(nextSpawnTime)
+	powerupSpawnTimer.start(minPowerupSpawnTime)
+
+func getRandomSpawnPos() -> Vector2:
+	var viewRect := get_viewport_rect()
+	var xPos := rand_range(viewRect.position.x, viewRect.end.x)
+	return Vector2(xPos, position.y)
 
 func _on_SpawnTimer_timeout():
 	# Spawn an enemy
-	var viewRect := get_viewport_rect()
-	var xPos := rand_range(viewRect.position.x, viewRect.end.x)
-	
 	if randf() < 0.1:
 		var meteor := plMeteor.instance()
-		meteor.position = Vector2(xPos, position.y)
+		meteor.position = getRandomSpawnPos()
 		get_tree().current_scene.add_child(meteor)
 	else:
 		var enemyPreload = preloadedEnemies[randi() % preloadedEnemies.size()]
 		var enemy: Enemy = enemyPreload.instance()
-		enemy.position = Vector2(xPos, position.y)
+		enemy.position = getRandomSpawnPos()
 		get_tree().current_scene.add_child(enemy)
 	
 	# Restart the timer
@@ -37,3 +46,10 @@ func _on_SpawnTimer_timeout():
 	if nextSpawnTime < MIN_SPAWN_TIME:
 		nextSpawnTime = MIN_SPAWN_TIME
 	spawnTimer.start(nextSpawnTime)
+
+func _on_PowerupSpawnTimer_timeout() -> void:
+	var powerupPreload = preloadedPowerups[randi() % preloadedPowerups.size()]
+	var powerup: Powerup = powerupPreload.instance()
+	powerup.position = getRandomSpawnPos()
+	get_tree().current_scene.add_child(powerup)
+	powerupSpawnTimer.start(rand_range(minPowerupSpawnTime, maxPowerupSpawnTime))
